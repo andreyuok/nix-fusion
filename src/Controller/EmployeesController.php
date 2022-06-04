de205dfc9c753385e9ae520dee7e5635a3fc013d
Andrey Borgoyakov <u.o.k.music@gmail.com>
2022-06-04 21:08:25 +0300

    Update EmployeesController.php

--- src/Controller/EmployeesController.php
+++ src/Controller/EmployeesController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/employees, name="employees")
+ * @Route("/employees", name="employees")
  */
 class EmployeesController extends AbstractController
 {
